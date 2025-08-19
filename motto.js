b2bb48d857943f18aa8ac0677886facef1016b36
404-Page-Found <Lucas20220605@gmail.com>
2025-08-19 13:26:49 +1000

    Updated motto.js

--- motto.js
+++ motto.js
@@ -4,5 +4,5 @@ const motto = [
   "The only way to do great work is to love what you do ———— Steve Jobs",
   "To be, or not to be, that is the question ———— Shakespeare",
   "All our dreams can come true, if we have the courage to pursue them ———— Walt Disney",
-  "The future belongs to those who believe in the beauty of their dreams ———— Eleanor Roosevelt"
+  "The future belongs to those who believe in the beauty of their dreams ———— Eleanor Roosevelt",
 ];
